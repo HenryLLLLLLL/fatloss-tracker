ccec6b4d1df2917d5b1812675342a3b4075b0673
HenryLLLLLLL <4chenryl@gmail.com>
2026-06-18 14:19:53 +0800

    cleanup: remove root-level PWA file duplicates (now under pwa/)

--- config.js
+++
@@ -1,9 +0,0 @@
-// FatLoss PWA Configuration
-const SUPABASE_CONFIG = {
-  url: 'https://qzmdcdwxleswncfspxkr.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6bWRjZHd4bGVzd25jZnNweGtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTkwODgsImV4cCI6MjA5NzIzNTA4OH0.j9FqyrEnvJjaJUnytK_W-mR4836tXDkW0F9xAHZR1us'
-};
-
-// Password protection: SHA-256 hash of your password
-// To change password, run: python3 -c "import hashlib; print(hashlib.sha256('YOUR_NEW_PASSWORD'.encode()).hexdigest())"
-const AUTH_HASH = 'cbc7f3b943942b9e45ed63595aaf50bbe9c5ab1756173b07fc3dbf59fc3a33e4';
